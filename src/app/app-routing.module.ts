1cdb345f5153a83321474f3f75f0666af0ee4a53
Shawnolo <szhou228@wisc.edu>
2018-12-09 14:53:02 -0600

    group management page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './pages/tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './pages/tabs/tabs.module#TabsPageModule' },
+  { path: 'GroupMNG', loadChildren: './group-mng/group-mng.module#GroupMNGPageModule' }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/group-mng/group-mng.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-group-mng',
+  templateUrl: './group-mng.page.html',
+  styleUrls: ['./group-mng.page.scss'],
+})
+export class GroupMNGPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
