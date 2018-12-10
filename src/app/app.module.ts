f489833ffa0a15651b6e59e9d4c75ba864a9afd2
NerdyTao <yewisc@gmail.com>
2018-12-09 22:48:20 -0600

    payment features paypal and venmo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,14 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// import { InAppBrowser } from '@ionic-native/in-app-browser'
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
+    // InAppBrowser,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
--- src/app/pages/contact/contact.page.ts
+++ src/app/pages/contact/contact.page.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+// import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 @Component({
   selector: 'app-contact',
   templateUrl: 'contact.page.html',
   styleUrls: ['contact.page.scss']
 })
-export class ContactPage {}
+export class ContactPage {
+  // constructor(
+  //   private inAppBrowser: InAppBrowser) {
+
+  // }
+  // redirectVenmo() {
+  //   this.inAppBrowser.create("https://www.w3schools.com/php/");
+  //  }
+
+}
 
 
